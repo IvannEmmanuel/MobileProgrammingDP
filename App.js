eb85a164a7f7c3d98798973bf118f9734a331729
IvannEmmanuel <F0dG1ngD13@gmail.com>
2024-09-05 01:02:30 +0800

    DONE

--- App.js
+++ App.js
@@ -1,116 +1,28 @@
-import React from 'react';
-import { Image, StyleSheet, Text, View, KeyboardAvoidingView, Platform, ScrollView, TouchableOpacity } from 'react-native';
-import { Button, Card, TextInput } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import LoginScreen from "./Pages/LoginScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    >
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.imageContainer}>
-          <Image source={require('./Images/FoxLogo.png')} style={styles.mainImage} /> 
-        </View>
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleLabel}>CunningCarat</Text>
-        </View>
-        <View style={styles.mainContainer}>
-          <Card style={styles.subContainer}>
-            <Text style={styles.emailLabel}> Email: </Text>
-            <TextInput 
-              placeholder="Username" 
-              style={styles.inputEmailAddress} 
-              mode="outlined"
-            />
-            <Text style={styles.passwordLabel}> Password: </Text>
-            <TextInput 
-              placeholder="Password" 
-              style={styles.inputPassword} 
-              secureTextEntry
-              mode="outlined"
-            />
-            <Button
-              mode="elevated"
-              style={styles.submitButton}
-            >
-              <Text style={styles.signInLabel}> Sign In </Text>
-            </Button>
-            <View style={styles.accountText}>
-              <Text>
-                Don't have an account?
-                  <Text style = {styles.signUpLabel}>
-                    {' '}
-                    Sign Up
-                  </Text>
-              </Text>
-            </View>
-          </Card>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <NavigationContainer>
+    <Stack.Navigator 
+    screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name='Login' component={LoginScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  signUpLabel: {
-    color: "#FCD12A",
-  },
-  accountText: {
-    alignItems: 'center'
-  },
-  signInLabel:{
-    fontSize: 18,
-    color: "white",
-  },
-  submitButton: {
-    marginVertical: 20,
-    backgroundColor: "#424242",
-    borderRadius: 10,
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  titleContainer: {
-    alignItems: "center",
-  },
-  titleLabel: {
-    fontSize: 35,
-    fontWeight: "800",
-    marginVertical: -40,
-  },
-  imageContainer: {
-    alignItems: "center",
-    marginVertical: 30,
-  },
-  mainContainer: {
-    marginVertical: 150,
-    marginTop: 30,
-    padding: 20,
-  },
-  subContainer: {
-    backgroundColor: "#F9F9F9",
-    padding: 22,
-    borderRadius: 25,
-  },
-  emailLabel: {
-    fontSize: 19,
-    marginBottom: 5,
-  },
-  inputEmailAddress: {
-    marginBottom: 10,
-    backgroundColor: "#F9F9F9",
-  },
-  passwordLabel: {
-    fontSize: 19,
-    marginBottom: 5,
-  },
-  inputPassword: {
-    backgroundColor: "#F9F9F9",
-  },
   container: {
     flex: 1,
-    backgroundColor: "#FFDC2E",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- Pages/LoginScreen.js
+++ Pages/LoginScreen.js
@@ -1,14 +1,145 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Image, StyleSheet, Text, View, KeyboardAvoidingView, Platform, ScrollView, TouchableOpacity } from 'react-native';
+import { Button, Card, TextInput } from 'react-native-paper';
 
-const LoginScreen = () => {
+function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.imageContainer}>
+          <Image source={require('../Images/FoxLogo.png')} style={styles.mainImage} /> 
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleLabel}>CunningCarat</Text>
+        </View>
+        <View style={styles.mainContainer}>
+          <Card style={styles.subContainer}>
+            <View style={styles.circle}>
+              <Image source={require('../Images/User.png')} style={styles.userImage}/>
+            </View>
+            <Text style={styles.emailLabel}> Email </Text>
+            <TextInput
+              placeholder="Username"
+              placeholderTextColor="#AFAFAF"
+              style={styles.inputEmailAddress}
+              mode="outlined"
+              theme={{ roundness: 20 }}
+            />
+            <Text style={styles.passwordLabel}> Password </Text>
+            <TextInput 
+              placeholder="Password"
+              placeholderTextColor="#AFAFAF"
+              style={styles.inputPassword} 
+              secureTextEntry
+              mode="outlined"
+              theme = {{ roundness: 20 }}
+            />
+            <Button
+              mode="elevated"
+              style={styles.submitButton}
+            >
+              <Text style={styles.signInLabel}> Sign In </Text>
+            </Button>
+            <View style={styles.accountText}>
+              <Text style = {{textDecorationLine: 'underline'}}>
+                Don't have an account?
+                  <Text style = {styles.signUpLabel}>
+                    {' '}
+                    Sign Up
+                  </Text>
+              </Text>
+            </View>
+          </Card>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainImage: {
+    width: 120,
+    height: 150,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+  },
+  signUpLabel: {
+    color: "#FCD12A",
+  },
+  accountText: {
+    alignItems: 'center'
+  },
+  signInLabel:{
+    fontSize: 17,
+    color: "white",
+  },
+  submitButton: {
+    marginVertical: 20,
+    backgroundColor: "#424242",
+    borderRadius: 10,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  titleLabel: {
+    fontSize: 30,
+    fontWeight: "700",
+    marginVertical: -40,
+  },
+  imageContainer: {
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  mainContainer: {
+    marginTop: 30,
+    padding: 20,
+  },
+  subContainer: {
+    backgroundColor: "#F9F9F9",
+    padding: 35,
+    borderRadius: 25,
+  },
+  emailLabel: {
+    fontSize: 17,
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  inputEmailAddress: {
+    marginBottom: 10,
+    backgroundColor: "#F9F9F9",
+  },
+  passwordLabel: {
+    fontSize: 17,
+    marginBottom: 5,
+  },
+  inputPassword: {
+    backgroundColor: "#F9F9F9",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#FFDC2E",
+  },
+  circle: {
+    position: 'absolute',
+    top: '-10%',
+    left: '50%',
+    transform: [{ translateX: -50 }, { translateY: -50 }],
+    width: 95,
+    height: 95,
+    borderRadius: 50,
+    backgroundColor: '#ACACAC',
+    alignItems: "center",
+    justifyContent: "center"
+  },
+});
